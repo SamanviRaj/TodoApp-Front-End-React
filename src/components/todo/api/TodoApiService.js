9b477c63e5b27c8bb382d9995ad1f582049f59ac
rthota7 <rthota7@dxc.com>
2023-07-25 22:33:00 +0530

    change in api client headers for base64 auth prototype

--- src/components/todo/api/TodoApiService.js
+++ src/components/todo/api/TodoApiService.js
@@ -7,18 +7,38 @@ const apiClient = axios.create(
 )
 
 export const retrieveTodos 
-        = () => apiClient.get("/api/v1/todo")
+        = () => apiClient.get("/api/v1/todo",{
+                headers: {
+                        Authorization : 'Basic cmFqZXNoOjEyMzQ1'
+                }
+        })
 
 export const retrieveTodosById 
-        = (id) => apiClient.get(`/api/v1/todo/get/${id}`)
+        = (id) => apiClient.get(`/api/v1/todo/get/${id}`,{
+                headers: {
+                        Authorization : 'Basic cmFqZXNoOjEyMzQ1'
+                }
+        })
         // /api/v1/todo/get/{id}
 
 export const updateTodoApi
-        = (todo) => apiClient.put("/api/v1/todo/update",todo)
+        = (todo) => apiClient.put("/api/v1/todo/update",todo,{
+                headers: {
+                        Authorization : 'Basic cmFqZXNoOjEyMzQ1'
+                }
+        })
 
 export const deleteTodoItem
-        = (id) => apiClient.delete(`/api/v1/todo/delete/${id}`)
+        = (id) => apiClient.delete(`/api/v1/todo/delete/${id}`,{
+                headers: {
+                        Authorization : 'Basic cmFqZXNoOjEyMzQ1'
+                }
+        })
         // /api/v1/todo/delete/203
 
 export const ceateTodoApi 
-        = (todo) => apiClient.post("/api/v1/todo/create",todo)
+        = (todo) => apiClient.post("/api/v1/todo/create",todo,{
+                headers: {
+                        Authorization : 'Basic cmFqZXNoOjEyMzQ1'
+                }
+        })
